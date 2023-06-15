c9e33cb96ba064bf2b2e8266442d47f168ce7a45
JesusOjeda35 <jaojedal@ufpso.edu.co>
2023-06-15 18:35:42 -0500

    The best pag

---
+++ js/myjs.js
@@ -0,0 +1,30 @@
+let ÁguilasD;
+let Millonarios = document.getElementById("Millonarios-FC");
+let AtleticoN = document.getElementById("Atletico-Nacional");
+let AméricaC;
+let BoyacáC;
+let AlianzaP;
+let IndependienteM;
+let DeportivoP;
+let Junior;
+let IndependienteSF;
+let Laequidad;
+let DeportivoPe;
+let DeportesT;
+let DeportivoC;
+let AtléticoB;
+let Envigado;
+let OnceC;
+let Jaguares;
+let UniónM; 
+let AtléticoH;
+function Nacional(){
+    if(document.getElementById("Atletico-Nacional")){
+        window.location.href = 'https://www.atlnacional.com.co/';
+    }
+}
+function MillonariosF(){
+    if(document.getElementById("Millonarios-FC")){
+        window.location.href = 'https://millonarios.com.co/';
+    }
+}
